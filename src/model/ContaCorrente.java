b48512ff409e5ef5232697248b127398dbb29b86
Vini <ovini.15515@gmail.com>
2024-08-21 11:11:30 -0300

    aula1

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -1,10 +1,27 @@
 package model;
 
 public class ContaCorrente {
-    int numero;
-    String cliente;
-    double saldo;
-    double limite;
+    public int numero;
+    public String cliente;
+    public double saldo;
+     public double limite;
+
+
+    public void saque(double quantidade){
+        if (quantidade <= saldo){
+            double novoSaldo = saldo - quantidade;
+            saldo = novoSaldo;
+            System.out.println("Novo saldo: " +saldo);
+        }else {
+            System.out.println("Valor inserido maior que o saldo");
+        }
+    }
+
+    public void depositar(double valor){
+        saldo = saldo + valor;
+        System.out.println("Novo saldo: " +saldo);
+    }
+
 
     public int getNumero() {
         return numero;
@@ -13,6 +30,32 @@ public class ContaCorrente {
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
 }
 
 
--- src/view/Correntista.java
+++ src/view/Correntista.java
@@ -5,6 +5,23 @@ import model.ContaCorrente;
 public class Correntista {
     public static void main(String[] args) {
         ContaCorrente Cliente = new ContaCorrente(); //criou o objeto "cliente"
+        Cliente.setNumero(503040);
+        Cliente.setCliente("Pedro");
+        Cliente.setSaldo(1000.00);
+        Cliente.setLimite(1500.00);
+        System.out.println("Número da conta: "+Cliente.getNumero());
+        System.out.println("Cliente: "+Cliente.getCliente());
+        System.out.println("Saldo: "+Cliente.getSaldo());
+        System.out.println("Limite: "+Cliente.getLimite());
+        Cliente.saque(100);
+
+        ContaCorrente Cliente1 = new ContaCorrente();
+        Cliente1.setNumero(123456);
+        Cliente1.setCliente("João");
+        Cliente1.setSaldo(1060.00);
+        Cliente1.setLimite(2000);
+        Cliente1.depositar(3500);
+
     }
 
 }
