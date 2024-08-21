0e9b66330bd5fc1f70fa8869da60bd21131d6c12
Vini <ovini.15515@gmail.com>
2024-08-21 09:51:52 -0300

    teste

---
+++ src/model/ContaCorrente.java
@@ -0,0 +1,19 @@
+package model;
+
+public class ContaCorrente {
+    int numero;
+    String cliente;
+    double saldo;
+    double limite;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
+
+
+
---
+++ src/view/Correntista.java
@@ -0,0 +1,10 @@
+package view;
+
+import model.ContaCorrente;
+
+public class Correntista {
+    public static void main(String[] args) {
+        ContaCorrente Cliente = new ContaCorrente(); //criou o objeto "cliente"
+    }
+
+}
